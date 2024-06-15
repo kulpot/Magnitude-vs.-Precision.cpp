bde63c47c5a6c48a215363a8766ef1eef6ef4c66
Kulpot <kulpotwahu@gmail.com>
2024-06-15 11:11:50 +0800

    void main() {  // 999999

--- Magnitude vs. Precision/CppScratchPad.cpp
+++ Magnitude vs. Precision/CppScratchPad.cpp
@@ -3,3 +3,8 @@ using std::cout;
 using std::endl;
 
 //Magnitude vs. Precision  //ref link:https://www.youtube.com/watch?v=v-C19K-OVCw&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=5
+
+void main()
+{
+	// 999999
+}
