91f990bc4f2759ad84145b9aae3f68f3b284d714
Kulpot <kulpotwahu@gmail.com>
2024-06-15 11:11:17 +0800

    Magnitude vs. Precision

--- Magnitude vs. Precision/CppScratchPad.cpp
+++ Magnitude vs. Precision/CppScratchPad.cpp
@@ -2,4 +2,4 @@
 using std::cout;
 using std::endl;
 
-//ref link:https://www.youtube.com/watch?v=v-C19K-OVCw&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=5
+//Magnitude vs. Precision  //ref link:https://www.youtube.com/watch?v=v-C19K-OVCw&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=5
